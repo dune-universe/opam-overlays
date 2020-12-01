350b3e96d116d1d352e18df6dc19acf5ae71a00b
Lucas Pluvinage <lucas@tarides.com>
2020-12-01 15:21:21 +0100

    when having multiple packages, CI tests last version.

--- list-overrides.sh
+++ list-overrides.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 if [ "$1" = "-bare" ]; then
-  find packages -type d -exec basename {} \; -mindepth 2 -maxdepth 2 | awk -F '.' '{print $1}'
+  find packages -type d -exec basename {} \; -mindepth 2 -maxdepth 2 | awk -F '.' '{print $1}' | sort | uniq
+elif [ "$1" = "-distinct" ]; then 
+  find packages -type d -exec basename {} \; -mindepth 2 -maxdepth 2 | sort -r | awk -F '.' '{ st = index($0,".");print substr($0,st+1) " " $1}' | uniq -f 1 | awk -F ' ' '{print $2 "." $1}' | sort
 else
-  find packages -type d -exec basename {} \; -mindepth 2 -maxdepth 2
+  find packages -type d -exec basename {} \; -mindepth 2 -maxdepth 2 | sort
 fi
