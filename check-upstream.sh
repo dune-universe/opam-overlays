08547dd14cb20f97417c3ae869589ea632506c08
Virgile Robles <virgile.robles@protonmail.ch>
2026-07-25 18:34:40 +0200

    shellcheck check-upstream.sh

--- check-upstream.sh
+++ check-upstream.sh
@@ -2,20 +2,20 @@
 
 OVERLAYS_REPO=${1:-overlays}
 
-PACKAGES=(`opam list --repo="$OVERLAYS_REPO" -A -s`)
+mapfile -t PACKAGES < <(opam list --repo="$OVERLAYS_REPO" -A -s)
 
 if [[ ${#PACKAGES[@]} -eq 0 ]]; then
-	echo "No package found for repository $OVERLAYS_REPO"
-	exit 2
+    echo "No package found for repository $OVERLAYS_REPO"
+    exit 2
 fi
 
 STATUS=0
 
-for pkg in `opam show -f package "${PACKAGES[@]}"`; do
-        barepkg=`echo $pkg | awk -F'+' '{print $1}'`
-	if ! [[ $pkg != $barepkg ]]; then
-		echo "New version upstream: $pkg"
-	fi
+for pkg in $(opam show -f package "${PACKAGES[@]}"); do
+    barepkg=$(echo "$pkg" | awk -F'+' '{print $1}')
+    if [[ $pkg == "$barepkg" ]]; then
+        echo "New version upstream: $pkg"
+    fi
 done
 
 exit $STATUS
